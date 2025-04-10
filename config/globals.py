85446a272a45f0da2a55862362e5b698df36e6c1
Pereverzev Marat <marat.pereverzrv@gmail.com>
2025-04-10 17:19:45 +0300

    add: authorization

---
+++ config/globals.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Config(BaseSettings):
+    DB_NAME: str
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: str
+    TELEGRAM_TOKEN: str
+
+    '''
+    @property
+    def sync_db_url(self):
+        return f"postgresql+psycopg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    model_config = SettingsConfigDict(env_file=".env")
+    '''
+
+config = Config()
--- main.py
+++ main.py
@@ -1,13 +1,44 @@
-import telebot
-from dotenv import dotenv_values
+from typing import Callable, Awaitable
 
-config = dotenv_values(".env")
+from aiogram import Bot, Dispatcher, types
+from aiogram.client.default import DefaultBotProperties
+from aiogram.filters import CommandStart, Command
+from telebot.types import Update
+from typing import Dict, Any, Awaitable
 
-bot = telebot.TeleBot(config.get("telegram_token"), parse_mode="markdown")
+from config.globals import config
+import asyncio
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+allowed_users = [686364607]
 
+default = DefaultBotProperties(parse_mode="Markdown")
+bot = Bot(token=config.TELEGRAM_TOKEN, default=default)
+dp = Dispatcher()
 
-bot.infinity_polling()
+@dp.message.middleware()
+async def database_transaction_middleware(
+    handler: Callable[[Update, Dict[str, Any]], Awaitable[Any]],
+    event: Update,
+    data: Dict[str, Any]
+) -> Any:
+    if data["event_from_user"].id not in allowed_users:
+        await bot.send_message(chat_id=data["event_chat"].id, text="⛔ You are not allowed to use this bot")
+        return
+    await handler(event, data)
+
+@dp.message(CommandStart())
+async def send_welcome(message: types.Message):
+    await message.answer("Привет! Добро пожаловать в AI TODODO! Выбери действие из списка команд")
+
+@dp.message(Command('id'))
+async def send_welcome(message: types.Message):
+    await message.answer(f"`{message.from_user.id}`")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Shutting down")
